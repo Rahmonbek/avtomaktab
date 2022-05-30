913a1b586ec9ca36096ad79d4c5d61111e9be2a9
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2022-05-30 10:13:36 +0500

    vsd

--- .history/src/App_20220530095058.js
+++ .history/src/App_20220530095058.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import 'antd/dist/antd.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default class App extends Component {
   render() {
     return (
